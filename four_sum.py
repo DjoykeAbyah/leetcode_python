aea31216d95a7e2b00dbdd487ba329767d37110a
DjoykeAbyah <crudendjoyke@gmail.com>
2024-09-22 14:47:30 +0200

    try out

--- four_sum.py
+++ four_sum.py
@@ -1,36 +1,31 @@
 class Solution(object):
-    def fourSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-        pair_map = {}
+    def four_sum(self, nums, target):
         result_set = []
+        pair_map = {}
         nums_len = len(nums)
 
         for i in range(nums_len - 1):
             for j in range(nums_len + 1, nums_len):
                 pair_sum = nums[i] + nums[j]
                 if pair_sum not in pair_map:
-                   pair_map[pair_sum] = []  # Creating an empty list if pair_sum key is not in the dictionary
-                pair_map[pair_sum].append(i, j) # appending the indexes as a tuple making it immutable
+                    pair_map[pair_sum] = []
+                    pair_map[pair_sum].append(i, j)
 
-        for i in range(nums_len - 1):
-            for j in range(nums_len + 1, nums_len):
-                current_sum = nums[i] + nums[j]
-                remain_sum = target - current_sum
+        for i in range(nums_len(nums - 1)):
+            for j in range(nums_len(nums + 1), n):
+                current_sum = mums[i] + nums[j]
+                leftover_sum = target - current_sum
 
-            if remain_sum in pair_map:
-                for pair in pair_map[remain_sum]:
-                    x, y = pair # tuple unpacking
-                    quadruplet = sorted[nums[i], nums[j], nums[x], nums[y]] # sorted to avoid duplicates
-                    result_set.add(tuple(quadruplet)) # Add as a tuple to avoid duplicates If you tried to add a list directly to a set, you would encounter a TypeError.
-        return list(quadruplet)	
-
-		
-			
-
-
-
+                if leftover_sum in pair_map:
+                    for pair in pair_map[leftover_sum]: # check every pair containing that sum
+                        x, y = pair
+                        quadruplet = sorted[sum[i], sum[j], sum[x], sum[y]]
+                        result_set.append(quadruplet)
         
+        return list(result_set)
+
+def test_four_sum()
+    assert solution.four_sum([1,0,-1,0,-2,2], 0) = [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+    print("all tests passed successfully")
+
+test_four_sum()
