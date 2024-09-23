52e187060307a555a47a13476ba263bdd8b5c68b
DjoykeReijans <crudendjoyke@gmail.com>
2024-09-23 15:39:26 +0200

    four sum

--- four_sum.py
+++ four_sum.py
@@ -1,34 +1,38 @@
 class Solution(object):
-    def four_sum(self, nums, target):
-        result_set = set()
-        pair_map = {}
-        nums_len = len(nums)
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """ 
+        nums.sort()  # sort array
+        result = []  # create list for result
+        n_len = len(nums)
 
-        for i in range(nums_len - 1):
-            for j in range(i + 1, nums_len):
-                pair_sum = nums[i] + nums[j]
-                if pair_sum not in pair_map:
-                    pair_map[pair_sum] = []
-                pair_map[pair_sum].append((i, j))
-
-        for i in range(nums_len - 1):
-            for j in range(i + 1, nums_len):
-                current_sum = nums[i] + nums[j]
-                leftover_sum = target - current_sum
-
-                if leftover_sum in pair_map:
-                    for pair in pair_map[leftover_sum]: # check every pair containing that sum
-                        x, y = pair
-                        if i != x and i != y and j != x and i != y:
-                            quadruplet = sorted([nums[i], nums[j], nums[x], nums[y]])
-                            result_set.add(tuple(quadruplet)) # use add 
-        result = []
-        for quadruplet in result_set:
-            result.append(list(quadruplet))
+        for i in range(n_len - 3):  # leave space for 3 other nums
+            if i > 0 and nums[i] == nums[i - 1]:  # if duplicate
+                continue
+            for j in range(i + 1, n_len - 2):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+				
+				# use two pointers for the remaining two numbers
+                left = j + 1
+                right = n_len - 1
+                while left < right:
+                    total = nums[i], nums[j], nums[left], nums[right]
+                    if total == target:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
+						# move pointers and skip duplicates
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+                        left += 1
+                        right -= 1
+                    elif total < target:
+                        left += 1
+                    else:
+                        right -= 1
         return result
-
-solution = Solution()
-
-print(solution.four_sum([1, 0, -1, 0, -2, 2], 0))
-# expected = [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
 
